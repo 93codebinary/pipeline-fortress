be123d2c3555f749cd8f714d3786e34caf40898a
sneha <sneha.kalakavaru.swaminathan@gmail.com>
2026-07-17 22:33:19 +0530

    Integrate helmet for improved security headers
    Added helmet middleware to enhance security by setting various HTTP headers.

--- app.ts
+++ app.ts
@@ -2,6 +2,15 @@
  * Copyright (c) 2014-2026 Bjoern Kimminich & the OWASP Juice Shop contributors.
  * SPDX-License-Identifier: MIT
  */
+/*17-July-2026/sneha/added helmet for remote headers
+Disable Express header Reduces Information Disclosure.
+Reduces ZAP findings:Missing headers,CSP,XSS protection,HSTS
+*/
+import helmet from "helmet"
+app.disable("x-powered-by")
+express-rate-limit
+
+app.use(helmet())
 
 async function app () {
   const { default: validateDependencies } = await import('./lib/startup/validateDependenciesBasic')
